a4f714f56eff2baea90a0c2b9b54e60abf6de4d5
SaiKumar Naidu <snaidu@intraedge.com>
2025-08-18 08:41:42 +0000

    updated dim customer model

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -14,10 +14,10 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as total_amount
 
     from orders
 
@@ -25,19 +25,35 @@ customer_orders as (
 
 ),
 
-final as (
+customer_tiers as (
 
+    select
+        customer_id,
+        case
+            when total_amount >= 1000 then 'gold'
+            when total_amount >= 500 then 'silver'
+            else 'bronze'
+        end as customer_tier
+
+    from customer_orders
+
+),
+
+
+final as(
     select
         customers.customer_id,
         customers.full_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders
+        as number_of_orders,
+        customer_tiers.customer_tier
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join customer_orders using (customer_id) 
+    left join customer_tiers using (customer_id)
 
 )
 
